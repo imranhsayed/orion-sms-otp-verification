eabb61f006aa44cbd99fc6c2897a74b3a6a6607d
Imran Sayed <imranhsayed@gmail.com>
2018-11-21 20:38:08 +0530

    Update Plugin files

---
+++ inc/woo-commerce-setting-template.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Orion Woo-commerce Setting Template
+ *
+ * @package Orion SMS OTP verification
+ */
+$text_domain = 'orion-sms-orion-sms-otp-verification';
+
+function ihs_text_for_translation( $text ) {
+	$text_domain = 'orion-sms-orion-sms-otp-verification';
+	return __( $text, $text_domain );
+}
+?>
+<div class="wrap orion-otp-mega-wrapper">
+	<div class="jumbotron">
+		<h6 class="mb-0 text-white lh-100">Upgrade to Orion SMS Plugin Woocommerce <i class="fab fa-product-hunt"></i>ro</h6>
+		<small><?php echo __( 'by', $text_domain ); ?> Imran Sayed, Smit Patadiya</small>
+	</div>
+	<div class="jumbotron">
+		<div class="container-fluid">
+			<img src="https://imransayed.com/orion/wp-content/uploads/2018/06/icon-128x128.png">
+			<h2 style="text-align:left"><i class="fa fa-info-circle" aria-hidden="true"></i> Description</h2>
+			<ul>
+				<li>Our Premium Versions.</li>
+				<li>An option for every pocket</li>
+			</ul>
+
+			<div class="ihs-pak-cont">
+				<div class="ihs-pak-cont-flex">
+
+					<div class="ihs-pak-item">
+						<!-- PRICE ITEM -->
+						<div class="panel price panel-red">
+							<div class="panel-heading  text-center">
+								<h3>BASIC</h3>
+								<p style="color: transparent; text-shadow: none;">Free</p>
+							</div>
+							<div class="panel-body text-center">
+								<p class="lead" ><strong>Free</strong></p>
+							</div>
+							<ul class="list-group list-group-flush text-center">
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  OTP Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  MSG91 support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  TWILIO support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  Single International Country</li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Multiple International Country</li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Transactional Route</li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Woocommerce Checkout OTP </li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Woocommerce Order SMS</li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Plugin Customization</li>
+							</ul>
+							<div class="panel-footer">
+								<a class="btn btn-lg btn-block btn-danger" target="_blank" href="https://wordpress.org/plugins/orion-sms-otp-verification/">GET NOW!</a>
+							</div>
+						</div>
+						<!-- /PRICE ITEM -->
+					</div>
+
+					<div class="ihs-pak-item">
+						<!-- PRICE ITEM -->
+						<div class="panel price panel-blue">
+							<div class="panel-heading arrow_box text-center">
+								<h3>PRO SUPREME</h3>
+								<p>With Woocommerce OTP</p>
+							</div>
+							<div class="panel-body text-center">
+								<p class="lead" ><strong>₹500 / $7.5</strong></p>
+							</div>
+							<ul class="list-group list-group-flush text-center">
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  OTP Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  MSG91 support</li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i>  TWILIO support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  Single International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Multiple International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Transactional Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> <span class="text-success">Woocommerce Checkout OTP( new )</span></li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Woocommerce Order SMS</li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Plugin Customization</li>
+							</ul>
+							<div class="panel-footer">
+								<a class="btn btn-lg btn-block btn-info" target="_blank" href="https://imransayed.com/orion/product/orion-pro-supreme/">GET NOW!</a>
+							</div>
+						</div>
+						<!-- /PRICE ITEM -->
+					</div>
+
+					<div class="ihs-pak-item">
+						<!-- PRICE ITEM -->
+						<div class="panel price panel-green">
+							<div class="panel-heading arrow_box text-center">
+								<h3>PRO VALUE PACK</h3>
+								<p>Woocommerce OTP + SMS</p>
+							</div>
+							<div class="panel-body text-center">
+								<p class="lead" ><strong>₹600 / $9</strong></p>
+							</div>
+							<ul class="list-group list-group-flush text-center">
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  OTP Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  MSG91 support</li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i>  TWILIO support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  Single International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Multiple International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Transactional Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> <span class="text-success">Woocommerce Checkout OTP( new )</span></li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> <span class="text-success">Woocommerce Order SMS( new )</span></li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Plugin Customization</li>
+							</ul>
+							<div class="panel-footer">
+								<a class="btn btn-lg btn-block btn-success" href="https://imransayed.com/orion/product/orion-pro-value-pack/">GET NOW</a>
+							</div>
+						</div>
+						<!-- /PRICE ITEM -->
+					</div>
+
+					<div class="ihs-pak-item">
+						<!-- PRICE ITEM -->
+						<div class="panel price panel-grey">
+							<div class="panel-heading arrow_box text-center">
+								<h3>THE ULTIMATE PACK</h3>
+							</div>
+							<div class="panel-body text-center">
+								<p class="lead" ><strong>₹1500 / $20</strong></p>
+							</div>
+							<ul class="list-group list-group-flush text-center">
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  OTP Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  MSG91 support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  TWILIO support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  Single International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Multiple International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Transactional Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> <span class="text-success">Woocommerce Checkout OTP( new )</span></li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> <span class="text-success">Woocommerce Order SMS( new )</span></li>
+								<li class="list-group-item text-danger"><i class="fa fa-times" aria-hidden="true"></i> Plugin Customization</li>
+							</ul>
+							<div class="panel-footer">
+								<a class="btn btn-lg btn-block btn-gold" target="_blank" href="https://imransayed.com/orion/product/orion-ultimate-pack/">GET NOW!</a>
+							</div>
+						</div>
+						<!-- /PRICE ITEM -->
+					</div>
+
+					<div class="ihs-pak-item">
+						<!-- PRICE ITEM -->
+						<div class="panel price panel-grey">
+							<div class="panel-heading arrow_box text-center">
+								<h3>PRO WITH CUSTOMIZATION</h3>
+							</div>
+							<div class="panel-body text-center">
+								<p class="lead" ><strong>₹2000 / $30 (min)</strong></p>
+							</div>
+							<ul class="list-group list-group-flush text-center">
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  OTP Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  MSG91 support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  TWILIO support</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i>  Single International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Multiple International Country</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Transactional Route</li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> <span class="text-success">Woocommerce Checkout OTP( new )</span></li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> <span class="text-success">Woocommerce Order SMS( new )</span></li>
+								<li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Plugin Customization</li>
+							</ul>
+							<div class="panel-footer">
+								<a class="btn btn-lg btn-block btn-gold" target="_blank" href="https://imransayed.com/orion/pro-with-customization/">GET NOW!</a>
+							</div>
+						</div>
+						<!-- /PRICE ITEM -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--1- Tutorial Section-->
+	<!--Heading-->
+	<div class="d-sm-flex align-items-center p-3 my-3 text-white-50 ihs-bg-light-purple rounded box-shadow" style="background-color: #6f42c1; box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, .05);">
+		<div class="lh-100 ihs-admin-head-cont">
+			<h6 class="mb-0 text-white lh-100">How to use the Plugin?</h6>
+			<small>Watch below demo tutorials to have a better understanding</small>
+		</div>
+	</div>
+	<div class="">
+		<div class="row">
+			<div class="col-md-4 col-sm-6 col-12">
+				<?php $description = __( 'The Plugin Now Supports both Twilio and msg91', $text_domain ); ?>
+				<?php ihs_get_video_cards( 'New feature | Twilio Support', $description, 'https://youtu.be/izQTwIvejh8' ); ?>
+			</div>
+			<div class="col-md-4 col-sm-6 col-12">
+				<?php $description = __( 'How to get API Key | SID | Auth Token | Twilio Phone No', $text_domain ); ?>
+				<?php ihs_get_video_cards( 'Generate Twilio API Key | SID | Auth Token | Twilio Phone No', $description, 'https://youtu.be/hne6x-8nbA0' ); ?>
+			</div>
+		</div>
+	</div>
+</div>
